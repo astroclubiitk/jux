fc1b39311346fccf9293122895292ceb43344a3c
Jadit19 <jadit19@gmail.com>
2022-10-30 02:57:58 +0530

    ready for pypi!

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
-    name='jux',
+    name='Jux',
     version='1.0.0',
-    description='',
-    long_description='',
+    description='Package for feature detection in Astronomy Lightcurves',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Astronomy Club, IITK',
     license='MIT',
     packages=find_packages(),
